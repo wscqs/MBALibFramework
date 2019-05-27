a056b31ba6212350999eb06126ee4cd3464d27d4
wscqs <935966764@qq.com>
2019-05-27 18:07:48 +0800

    init

---
+++ MBALibFramework/MBALibFramework/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MBALibFramework
+//
+//  Created by 1 on 2019/5/27.
+//  Copyright © 2019 1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MBALibFramework/MBALibFramework/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MBALibFramework
+//
+//  Created by 1 on 2019/5/27.
+//  Copyright © 2019 1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
